b8c79daf0438c9d53bbbf8a85589f73cde9afc51
Alexey <koldaev.alexey@gmail.com>
2022-09-11 15:52:12 +0000

    Update 3 files

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import {useDispatch} from "react-redux"
 import { Navigate } from "react-router-dom";
+import { useAuth } from "hooks/use-auth";
+import { removeUser } from "../store/slices/userSlice"
 
 const HomePage = () => {
-    return <Navigate to="/login" />;
+    const {isAuth, email} = useAuth();
+    const dispatch = useDispatch();
+    
+    return isAuth ? (
+        <div>
+            <h1>WELCOME</h1>
+            <button
+            onClick={()=> dispatch(removeUser())}
+            >Log out from {email}</button>
+        </div>
+    ) : (<Navigate to="/login" />);
 };
 
 export default HomePage;
